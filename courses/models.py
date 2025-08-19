c326b05d52e3a655b180f358e662b4fbde6c40a4
LifelongWay <amin.abuhilga.140705@gmail.com>
2025-08-19 16:00:34 +0300

    Department model added

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,40 @@
 from django.db import models
-
+from departments.models import Department
 # Create your models here.
+class Course(models.Model):
+    # department related
+    department = models.ForeignKey(
+        Department,
+        on_delete=models.CASCADE,
+        blank = False,
+        null = False,
+        related_name= 'courses'
+        )
+
+    # recommended primary key
+    course_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length = 20, blank = False)
+    credits = models.IntegerField()
+    prerequisite_courses = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        blank = True,
+        related_name = 'prerequisites'
+        )
+
+class Section(models.Model):
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    SEMESTER_CHOICES = [
+        ('spring', 'Spring'),
+        ('fall', 'Fall'),
+        ('summer', 'Summer')
+    ]
+
+    section_number = models.IntegerField(blank = False, null = False)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name= 'sections')
+    capacity = models.PositiveSmallIntegerField(default = 0)
+    year = models.IntegerField(blank = False, null = False)    
+    semester = models.CharField(choices=SEMESTER_CHOICES, blank = False, null = False)
+    
+    class Meta:
+        unique_together = ('course', 'section_number', 'year', 'semester')
--- departments/models.py
+++ departments/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Department(models.Model):
+    dept_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length= 20, unique = True, blank = False, null = False)
