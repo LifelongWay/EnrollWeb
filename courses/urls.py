fff3104fea4ccb94b480f131e6cc260d987e3684
LifelongWay <amin.abuhilga.140705@gmail.com>
2025-08-27 00:42:49 +0300

    added first change

--- courses/urls.py
+++ courses/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 from . import views
-from django.conf import settings 
-from django.conf.urls.static import static
+
 app_name = 'courses'
 
 urlpatterns = [
-    path('panel/', views.courses_and_sections_view, name = 'panel'), # management panel - should be secured
-    path('panel/add/<str:type>/', views.courses_and_sections_view, name = 'add'),
-    path('panel/delete/<str:type>/<int:pk>', views.courses_and_sections_delete_view, name = 'delete'),
-    path('panel/edit/<str:type>/<int:pk>', views.courses_and_sections_edit_view, name = 'edit'),
 
+        path('panel/', views.courses_and_sections_view, name = 'panel'), # management panel - should be secured
+        path('panel/add/<str:type>/', views.courses_and_sections_view, name = 'add'),
+        path('panel/delete/<str:type>/<int:pk>', views.courses_and_sections_delete_view, name = 'delete'),
+        path('panel/edit/<str:type>/<int:pk>', views.courses_and_sections_edit_view, name = 'edit'),
     ]
